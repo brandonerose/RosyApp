693b7034026e324506fb33738a14144cdf3fc9be
brandonerose <brandon.ed.rose@gmail.com>
2024-10-30 15:25:54 -0400

    dev

--- R/tables.R
+++ R/tables.R
@@ -83,7 +83,7 @@ get_labels <- function(DF){
 }
 #' @title make_DT_table
 #' @export
-make_DT_table<-function(DF,editable = F,selection="single",paging = TRUE,scrollY = T,searching = T){
+make_DT_table<-function(DF,editable = F,selection="single",paging = TRUE,scrollY = F,searching = T){
   if(!is_something(DF)){
     return(h3("No data available to display."))
   }
@@ -91,7 +91,7 @@ make_DT_table<-function(DF,editable = F,selection="single",paging = TRUE,scrollY
     selection = selection,
     editable = editable,
     rownames = F,
-    fillContainer = T,
+    # fillContainer = T,
     # extensions = 'Buttons',
     options = list(
       columnDefs = list(list(className = 'dt-center',targets = "_all")),
@@ -103,14 +103,14 @@ make_DT_table<-function(DF,editable = F,selection="single",paging = TRUE,scrollY
       scrollX = T,
       # autoWidth = T,
       searching = searching,
-      dom = 'Bfrtip',
+      # dom = 'Bfrtip',
       # buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
       scrollCollapse = F,
       stateSave = F
     ),
     class = "cell-border",
-    filter = 'top',
-    escape = F
+    filter = 'top'
+    # escape = F
   ) %>% DT::formatStyle(
     colnames(DF),
     color = "#000"
@@ -126,12 +126,4 @@ make_DT_table_simple<-function(DF){
     colnames(DF),
     color = "#000"
   ) %>% return()
-}
-#' @title make_DT_table_simpler
-#' @export
-make_DT_table_simpler<-function(DF){
-  if(!is_something(DF)){
-    return(h3("No data available to display."))
-  }
-  DF %>% DT::datatable() %>% return()
 }
