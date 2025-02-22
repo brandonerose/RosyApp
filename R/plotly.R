b467acce54233127b5c4ea608de163b31d2ccbd8
brandonerose <brandon.ed.rose@gmail.com>
2025-02-22 16:17:14 -0500

    dev

--- R/plotly.R
+++ R/plotly.R
@@ -190,3 +190,26 @@ plotify<-function(GG){
                    )
     )
 }
+#' @title make_parcats
+#' @export
+make_parcats<-function(
+    DF,
+    remove_missing = F,
+    marginal_histograms = T
+){
+  if(remove_missing){
+    DF <- na.omit(DF)
+  }
+  colnames(DF) <-  colnames(DF) %>% lapply(function(col) {
+    x<- attr(DF[[col]], "label")
+    ifelse(is.null(x),col,x)
+  }) %>% unlist()
+  parcats::parcats(
+    easyalluvial::alluvial_wide(DF,NA_label = "*Missing*"),
+    marginal_histograms = marginal_histograms,
+    data_input = DF,
+    labelfont=list(size = 12,color = "black"),
+    tickfont=list(size = 12,color = "black"),
+    arrangement = "freeform"
+  )
+}
